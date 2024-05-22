1b439b4849a50a12a16c883bca4cdc6271cd8a19
nikitapoludencev <poludencevnikita@gmail.com>
2024-05-22 18:23:10 +0300

    Подключение коллекции games из Mongoose

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
-
+const usersRouter = require("./routes/users");
+const gamesRouter = require("./routes/games");
+const categoriesRouter = require("./routes/categories");
+const { connect } = require("http2");
+const connectToDataBase = require("./database/connect");
 
 
 const PORT = 3000;
 const app = express();
+connectToDataBase();
 
 app.use(
     bodyParser.json(),
     express.static(path.join(__dirname, "public")),
     usersRouter,
     gamesRouter,
-    categorRouter
+    categoriesRouter
 );
 
 app.listen(PORT, () => {
